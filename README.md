c5ee7ff4c0095fe8ce446b052715f75586aba77c
Vansh Sharma <vanshsharma9354@gmail.com>
2022-01-29 15:07:34 +0000

    Refact: Refactor the old code.
    Description: Added the Readme file

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-CLI App
+<div align="center"><h1> HTML CSS Quiz ? </h1></div>
 
-# HTML/CSS Quiz
- If you want to check your knowledge of HTML/CSS then this quiz is for you. With 15 questions and 3 Levels. 
+This is simple CLI quiz App.
 
-Module:
+This quiz has 15 questions about HTML CSS for every correct question you will be awarded with  2 points and for every wrong answer 1 point will be deducted.
 
-1. readline-sync
-
-1. CHALK.
+This quiz has 3 Levels.
+To go to level 2 you will have to score  5+ points and to go for level 3 you will have to score 10+ points.
 
 
-## Check it out here: [HTML/CSS quiz](https://replit.com/@sharmavansh/HTML-CSS-Quiz-M2#index.js?embed=1&output=1)
 
+
+## [LIVE APP](https://replit.com/@sharmavansh/HTML-CSS-Quiz-M2#index.js?embed=1&output=1)
+
+---
+![Demo Image](./images/demo.png)
+
+---
+
+<h3> Tech used: </h3>
+1. HTML<br/>
+2. CSS<br/>
+3. JavaScript<br/>
+4. Chalk package for colors 
+
+---
+
+Contact :
+
+- [Twitter](https://twitter.com/Vanshsh2701)
+- [Linkedin](https://www.linkedin.com/in/vanshsharma27/)
--- index.js
+++ index.js
@@ -1,202 +1,222 @@
-//Module
-var readLine =require("readline-sync");
-const chk=require("chalk");
-const log=console.log;
-const quesColor=chk.cyanBright;
-const answColor=chk.green;
+const ask = require("readline-sync");
+const chalk = require("chalk");
 
-//Opening
-
-if( readLine.keyInYN("Do you want to play this Quiz? "))
-{
-  var name=readLine.question("Please write your Name: ")
-  }else
-{
-  log('Exit');
-  process.exit()
-}
-
-log(chk.bgBlue(` Welcome!    \n ${chk.white.underline(name)} Best of Luck!\n Level 1        \n\n`));
-
-var score=0;
+const quesColor = chalk.cyanBright;
+const answColor = chalk.green;
 
 
-// Questions List
-const quesList=
-[
-  {
-    ques:`"Select the correct HTML for creating a hyperlink?"
-         a: A url="http://www.xyz.com"
-         b: A name="http://www.xyz.com">xyz.com
-         c: <A href="http://www.xyz.com">xyz</A>\n`,
-    answ:"c"
-  },
-  {
-    ques:`"In CSS, what is the correct option to select all the tags on a page? "
+const quesList =
+  [
+    {
+      ques: `Q1. Select the correct HTML for creating a hyperlink?
+         a: a url="http://www.xyz.com"
+         b: <a name="http://www.xyz.com"> xyz.com
+         c: <a href="http://www.xyz.com"> xyz </a>
+         `,
+      answ: "c"
+    },
+    {
+      ques: `Q2. In CSS, what is the correct option to select all the tags on a page? 
          a:  p{}
          b: .p{}
-         c: <p>{}\n`,
-    answ:"a"
-  },
-  {
-    ques:`"Select the option to make a list that lists the items with bullets?"
-         a: Ol
-         b: Dl
-         c: List\n`,
-    answ:"b"
-  },
-  {
-    ques:`"In CSS, choose the correct option to select this image by its id?
-
-<img id=”mainpic” src=”cat.png”>"
+         c: <p>{}
+         `,
+      answ: "a"
+    },
+    {
+      ques: `Q3. Select the option to make a list that lists the items with bullets?
+         a: ol
+         b: dl
+         c: List
+         `,
+      answ: "b"
+    },
+    {
+      ques: `Q4. In CSS, choose the correct option to select this image by its id?
+<img id="mainpic" src="cat.png">
+       
          a: img{}
          b: .mainpic{}
-         c: #mainpic\n`,
-    answ:"c"
-  },
-  {
-    ques:`"In CSS, Select the appropriate option to style an element so that the next element would appear right next to it not underneath it if both elements widths were collectively smaller than the container element?"
+         c: #mainpic 
+         `,
+      answ: "c"
+    },
+    {
+      ques: `Q5. In CSS, Select the appropriate option to style an element so that the next element would appear right next to it not underneath it if both elements widths were collectively smaller than the container element?
          a: None
          b: display:inline
-         c: display:left\n`,
-    answ:"b"
-  },
-  {
-    ques:`"Select the correct HTML tag to make a text bold."
+         c: display:left
+         `,
+      answ: "b"
+    },
+    {
+      ques: `Q6. Select the correct HTML tag to make a text bold.
          a: b
          b: bb
-         c: bold\n`,
-    answ:"a"
-  },
-  {
-    ques:`"Select the appropriate HTML tag for inserting a line break?"
+         c: bold
+         `,
+      answ: "a"
+    },
+    {
+      ques: `Q7. Select the appropriate HTML tag for inserting a line break?
          a: break
          b: brk
-         c: br\n`,
-    answ:"c"
-  },
-  {
-    ques:`"Select the correct option to open a link in a new browser window?"
+         c: br
+         `,
+      answ: "c"
+    },
+    {
+      ques: `Q8. Select the correct option to open a link in a new browser window?
          a: A href="url"target="new"
          b: A href="url"target="_blank"
-         c: A href="url"target="window"\n`,
-    answ:"b"
-  },
-  {
-    ques:`"Select the property used to create space between the element’s border and inner content?"
+         c: A href="url"target="window"
+         `,
+      answ: "b"
+    },
+    {
+      ques: `Q9. Select the property used to create space between the element’s border and inner content?
          a: border
          b: padding
-         c: spacing\n`,
-    answ:"b"
-  },
-  {
-    ques:`"In CSS,Select the property used to set the spacing in between lines of text?"
+         c: spacing
+         `,
+      answ: "b"
+    },
+    {
+      ques: `Q10. In CSS,Select the property used to set the spacing in between lines of text?
          a: line-spacing
          b: letter-spacing
-         c: line-height\n`,
-    answ:"c"
-  },
-  {
-    ques:`"Select the property that is used to change the list style to show roman numerals instead of normal numbers?"
+         c: line-height
+         `,
+      answ: "c"
+    },
+    {
+      ques: `Q11. Select the property that is used to change the list style to show roman numerals instead of normal numbers?
          a: list-style-type:upper-roman:
          b: list-style-type:roman-numerals;
-         c: list-style:roman;\n`,
-    answ:"a"
-  },
-  {
-    ques:`"In CSS, Select the property to set an image in a list instead of a standard bullet?"
+         c: list-style:roman;
+         `,
+      answ: "a"
+    },
+    {
+      ques: `Q12. In CSS, Select the property to set an image in a list instead of a standard bullet?
          a: list-image
          b: image-list
-         c: list-style-image\n`,
-    answ:"c"
-  },
-  {
-    ques:`"Select the property that is used to create spacing between HTML elements?"
+         c: list-style-image
+         `,
+      answ: "c"
+    },
+    {
+      ques: `Q13. Select the property that is used to create spacing between HTML elements?
          a: margin
          b: padding
-         c: none\n`,
-    answ:"b"
-  },
-  {
-    ques:`"In CSS,select the property used to set the background color of an image?"
+         c: none
+         `,
+      answ: "b"
+    },
+    {
+      ques: `Q14. In CSS,select the property used to set the background color of an image?
          a: background-color
          b: color
-         c: background:color\n`,
-    answ:"a"
-  },
-  {
-    ques:`"For users that use the tab key to navigate websites, what property represents this way of moving from one element to another?"
+         c: background:color
+         `,
+      answ: "a"
+    },
+    {
+      ques: `Q15. For users that use the tab key to navigate websites, what property represents this way of moving from one element to another?
          a: a:focus
          b: a:active
-         c: a:visited\n`,
-    answ:"a"
-  },
-];
+         c: a:visited
+         `,
+      answ: "a"
+    },
+  ];
 
 
 
+if (ask.keyInYN(chalk.red("Do you want to play this Quiz? "))) {
 
-//Function
-function play(ques,answ)
-{
-  var isAnswerCorrect;
+  let userName = ask.question("Please write your Name: ")
 
-  var answer=readLine.question(quesColor(ques));
+  console.log(chalk.blue.underline.bold(`
 
-  if(answer.toLocaleLowerCase()===answ.toLocaleLowerCase())
- {
-   isAnswerCorrect=true;
-   score+=2;
- } 
- else
- {
-   isAnswerCorrect=false;
-   score-=1;
- }
- return isAnswerCorrect;
+Welcome ${userName}
+Best of Luck !!
+Level 01
+`));
+
+} else {
+
+  console.log('Exit');
+
+  process.exit()
+
 }
 
 
-//Loop
-for(var i=0; i<quesList.length;i++){
-  if(i==5&&score>=7){
-     log(chk.bgBlue("Welcome!\nLevel 2 "));
-   }
-  if(i==5&&score<7)
-    {
-     log(chk.bgRed("Sorry You Can't make it to Level 2\n Better Luck Next Time."));
-     break;
-    }
-   var currentQuestion=quesList[i];
-   var check=play(currentQuestion.ques,currentQuestion.answ);
-   if(check===true){
+let score = 0;
 
-      log(chk.black.bgGreenBright("GREAT!"));
-      log(chk.yellowBright(`Your Score is: ${score}\n\n`));
-      log(chk.blueBright("****************************"))
-    }
-   else{  
-     log(chk.bgRed("Opps!"))
-     log(`Right answer is: ${answColor(currentQuestion.answ)}`)
-     log(chk.yellowBright(`Your Score is: ${score}\n\n`))
-     log(chk.blueBright("****************************"))
-    }
-   if(i==10&&score>=16){
-     log(chk.bgBlue("Welcome!\nLevel 3 "));
-     
-    }
-  if(i==10&&score<16)
-  {
-    log(chk.bgBlue("Sorry You can't make it to Level 3\nBetter Luck Next Time"));
-    break;
+
+for (let i = 0; i < quesList.length; i++) {
+  if (i == 5 && score >= 7) {
+    console.log(chalk.bgBlue(` Welcome! Level 2 `));
   }
-  
+  if (i == 5 && score < 7) {
+
+    console.log(chalk.bgRed(` Sorry You Can't make it to Level 2!  Better Luck Next Time.`))
+    return
+
+  }
+
+  let currentQuestion = quesList[i];
+  let check = play(currentQuestion.ques, currentQuestion.answ);
+
+  if (check === true) {
+
+    console.log(chalk.black.bgGreenBright("GREAT! "));
+    console.log(chalk.yellowBright(`Your Score is: ${score}`));
+    console.log(chalk.blueBright("****************************"))
+  }
+  else {
+    console.log(chalk.bgRed("Opps! "))
+    console.log(`Right answer is: ${answColor(currentQuestion.answ)}`)
+    console.log(chalk.yellowBright(`Your Score is: ${score}`))
+    console.log(chalk.blueBright("****************************"))
+
+  } if (i == 10 && score >= 16) {
+    console.log(chalk.bgBlue("Welcome! Level 3 "));
+
+  } if (i == 10 && score < 16) {
+    console.log(chalk.bgBlue(`Sorry You can't make it to Level 3 Better Luck Next Time`));
+    return;
+  }
+
 }
 
-//Fianl Result
-log(chk.bgBlue(`Final Score is: ${score}\nOut of: 30   `))
-log(chk.blueBright("---------------------------------------------------------------"))
 
-  
+function play(ques, answ) {
+  let isAnswerCorrect;
+
+  let answer = ask.question(quesColor(ques));
+
+  if (answer.toLocaleLowerCase() === answ.toLocaleLowerCase()) {
+    isAnswerCorrect = true;
+    score += 2;
+  }
+  else {
+    isAnswerCorrect = false;
+    score -= 1;
+  }
+  return isAnswerCorrect;
+}
+
+
+
+
+console.log(chalk.bgBlue(`Final Score is: ${score}
+Out of: 30 `))
+
+console.log(chalk.blueBright("---------------------------------------------------------------"))
+
+
 
 
